267db89154ba751e8da0f01f6d98555768a4b072
Paulo Silva Reis Vulpi <reispaulosilva@gmail.com>
2026-04-18 04:20:17 -0400

    chore(_shared): barrel exports para components/

---
+++ app/_shared/components/index.ts
@@ -0,0 +1,8 @@
+export { DisclaimerModal } from './DisclaimerModal';
+export type { DisclaimerModuleType } from './DisclaimerModal';
+export { UploadCard } from './UploadCard';
+export { UncertaintyBanner } from './UncertaintyBanner';
+export { ModuleCard } from './ModuleCard';
+export { DownloadPhotoButton } from './DownloadPhotoButton';
+export { PresentationModeToggle } from './PresentationModeToggle';
+export { PresentationModeProvider, usePresentationMode } from './PresentationModeProvider';
